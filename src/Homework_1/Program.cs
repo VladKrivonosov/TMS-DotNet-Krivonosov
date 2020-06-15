93379b62cc19cbbd6ef003534b3dca499a06b409
VladKrivonosov <wonin08@gmail.com>
2020-06-15 20:14:52 +0300

    fix: changed project way

--- src/Homework_1/Program.cs
+++ src/Homework_1/Program.cs
@@ -1,47 +1,60 @@
 ﻿using System;
 
-namespace homework1
+namespace Homework_1
 {
+    enum Days
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+
     class Program
     {
-        enum Week
-        {
-            None = 0,
-            Monday = 1,
-            Tuesday = 2,
-            Wednesday = 3,
-            Thursday = 4,
-            Friday = 5,
-            Saturday = 6,
-            Sunday = 7
-        }
-
         static void Main(string[] args)
         {
+            Console.Write("Hello, please enter the day number of the week, \n" +
+                "or day name (Mo, Tu, We etc) : ");
+            string DayOfWeek = Console.ReadLine();
 
+            bool result = int.TryParse(DayOfWeek, out int NumerOfWeek);
 
-            try
+            if (DayOfWeek == "Mo" || NumerOfWeek == 1)
             {
-                int day;
-                Console.WriteLine("enter the day number of the week:");
-                day = Convert.ToInt32(Console.ReadLine());
-
-                if (day <= 7 && day >= 1)
-                {
-                    var dayOfWeek = (Week)day;
-
-                    Console.WriteLine(day + " day of week it is: \n" + dayOfWeek.ToString());
-                }
-                else
-                    Console.WriteLine("invalid weekday number \nenter from 1 to 7");
-
+                Console.WriteLine("Monday is the 1 day of week.");
             }
-
-            catch
+            else if (DayOfWeek == "Tu" || NumerOfWeek == 2)
             {
-                Console.WriteLine("The entered value is not a number");
+                Console.WriteLine("Tuesday is the 2 day of week.");
             }
-            Console.ReadKey();
+            else if (DayOfWeek == "We" || NumerOfWeek == 3)
+            {
+                Console.WriteLine("Wednesday is the 3 day of week.");
+            }
+            else if (DayOfWeek == "Th" || NumerOfWeek == 4)
+            {
+                Console.WriteLine("Thursday is the 4 day of week.");
+            }
+            else if (DayOfWeek == "Fr" || NumerOfWeek == 5)
+            {
+                Console.WriteLine("Friday is the 5 day of week.");
+            }
+            else if (DayOfWeek == "Sa" || NumerOfWeek == 6)
+            {
+                Console.WriteLine("Saturday is the 6 day of week.");
+            }
+            else if (DayOfWeek == "Su" || NumerOfWeek == 7)
+            {
+                Console.WriteLine("Sunday is the 7 day of week.");
+            }
+            else
+            {
+                Console.WriteLine("You entered something wrong.");
+            }
         }
     }
 }

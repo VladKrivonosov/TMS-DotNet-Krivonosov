83d1907ac839065eed29c310865a20dca2dac4b2
VladKrivonosov <wonin08@gmail.com>
2020-06-25 01:07:37 +0300

    feat: done HW3

--- src/Homework_3/MainWindow.xaml.cs
+++ src/Homework_3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Homework_3.services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,48 @@ namespace Homework_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDateList.json";
+        private BindingList<models.TodoModel> _todoDateList;
+        private FileIOServices _fileIOServices;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOServices = new FileIOServices(PATH);
+
+            try
+            {
+                _todoDateList = _fileIOServices.LoadDate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+            
+            dgTodoList.ItemsSource = _todoDateList;
+            _todoDateList.ListChanged += _todoDateList_ListChanged;
+        }
+
+        private void _todoDateList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                   _fileIOServices.SaveDate(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
         }
     }
 }
